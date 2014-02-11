b7294803a298171219e9eb74d51defccb13f61e0
cdreysch <cdrey@gmx.de>
2014-02-11 18:40:24 +0100

    planted some flowers :)
    made some good progress by using this tile map example:
    http://www.sfml-dev.org/tutorials/2.0/graphics-vertex-array.php
    
    the view class now handles all the zoom and camera movement
    
    the fullscreen mode is now  fixed to 0 (native screen resolution) and
    the view rectangle is scaled to fit the screen ratio. this way the
    zoomed pixel should stay sharp
    
    Hugoo has learned some new skills:
    l - move right
    h - move left
    j - stop
    f - plant flower
    t - camera follow Hugoo
    w,s,a,d - move camera freely

--- src/main.cpp
+++ src/main.cpp
@@ -52,26 +52,137 @@ public:
 	}
 };
 
+class myTileMap : public sf::Drawable, public sf::Transformable{
+public:
+	bool load(const std::string& tileset, sf::Vector2u tileSize, std::vector<int>* tiles, unsigned int width, unsigned int height)    {
+		// load the tileset image
+		sf::Image tilesetImage;
+		if (!tilesetImage.loadFromFile(tileset))
+			return false;		
+		tilesetImage.createMaskFromColor(tilesetImage.getPixel(0,0));
+
+		// load the tileset texture
+		if (!m_tileset.loadFromImage(tilesetImage))
+			return false;
+
+		// resize the vertex array to fit the level size
+		m_vertices.setPrimitiveType(sf::Quads);
+		m_vertices.resize(width * height * 4);
+
+		// populate the vertex array, with one quad per tile
+		for (unsigned int i = 0; i < width; ++i)
+			for (unsigned int j = 0; j < height; ++j){
+
+				// get the current tile number
+				int tileNumber = tiles->at(i + j * width);
+
+				// find its position in the tileset texture
+				int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+				int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+				// get a pointer to the current tile's quad
+				sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+				// define its 4 corners
+				quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+				quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+				quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+				quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+				// define its 4 texture coordinates
+				quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+				quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+				quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+				quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+			}
+
+			m_tilesize = tileSize;
+			m_width = width;
+			m_height = height;
+			return true;
+	}
+
+	int changeTile(sf::Vector2f pos, unsigned int tileNumber)    {
+		int x = (int)pos.x/m_tilesize.x;
+		int y = ((int)pos.y/m_tilesize.y)+2;
+
+		// find its position in the tileset texture
+		int tu = tileNumber % (m_tileset.getSize().x / m_tilesize.x);
+		int tv = tileNumber / (m_tileset.getSize().x / m_tilesize.x);
+
+		// get a pointer to the tile's quad
+		sf::Vertex* quad = &m_vertices[(x + y * m_width) * 4];
+
+		// define its 4 texture coordinates
+		quad[0].texCoords = sf::Vector2f(tu * m_tilesize.x, tv * m_tilesize.y);
+		quad[1].texCoords = sf::Vector2f((tu + 1) * m_tilesize.x, tv * m_tilesize.y);
+		quad[2].texCoords = sf::Vector2f((tu + 1) * m_tilesize.x, (tv + 1) * m_tilesize.y);
+		quad[3].texCoords = sf::Vector2f(tu * m_tilesize.x, (tv + 1) * m_tilesize.y);
+		return x;
+	}
+
+private:
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const    {
+
+		// apply the transform
+		states.transform *= getTransform();
+
+		// apply the tileset texture
+		states.texture = &m_tileset;
+
+		// draw the vertex array
+		target.draw(m_vertices, states);
+	}
+
+	sf::VertexArray m_vertices;
+	sf::Texture m_tileset;
+	sf::Vector2u m_tilesize;
+	unsigned int m_width; 
+	unsigned int m_height;
+};
+
 int main()
 {
-	int width = 640;
-	int height = 480;	
-	int gridsize_texture = 16;
-	int gridsize_display = 32;
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// VideoModes
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// Display the list of all the video modes available for fullscreen
-	int fullscreenModeIndex=0;
 	std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
-	for (std::size_t i = 0; i < modes.size(); ++i) {
-		sf::VideoMode mode = modes[i];
-		std::cout << "Mode #" << i << ": " << mode.width << "x" << mode.height << " - " 
-			<< mode.bitsPerPixel << " bpp - 16:" << (int)(16.f*(float)mode.height/(float)mode.width);		
-		if(mode.width==width&&mode.height==height&&mode.bitsPerPixel==32){
-			fullscreenModeIndex = i;
-			std::cout << "  <-- selected";
-		}
-		std::cout << std::endl;
-	}
+	int fullscreenModeIndex=0;	
+	float fullscreenRatio = (float)modes[fullscreenModeIndex].width/(float)modes[fullscreenModeIndex].height;
+	sf::VideoMode fullscreenMode = modes[fullscreenModeIndex];	
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	//  Window
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int window_width = 640;
+	int wondow_height = (int)(640.f/fullscreenRatio);
+	sf::VideoMode windowedMode = sf::VideoMode(window_width, wondow_height);
+	sf::RenderWindow window(windowedMode, "Hugoo");	
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// View
+	/////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	float zoom = 2.f;
+	sf::View view;	
+	view.reset(sf::FloatRect(0, 0, window_width/zoom, (float)window_width/(zoom*fullscreenRatio)));
+	bool toggleViewFollowHugoo = true;
+	bool isFullscreen = false;
+	window.setView(view);		
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// FPS Display
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	sf::Int64 totalFrames = 0;
+	sf::Int32 elapsedFrames = 0;
+	sf::Int32 fpsUpdateFrequency = 10; //Hertz
+	sf::Int32 frameratelimit = 50;
+	sf::Clock clock;
+
+	window.setFramerateLimit(frameratelimit);
 
 	sf::Font font;
 	if (!font.loadFromFile(std::string("src/fonts/FreeMono.ttf").insert(0,mainpath))){
@@ -82,40 +193,52 @@ int main()
 	text.setCharacterSize(12); // in pixels, not points!
 	text.setColor(sf::Color(50,50,50));
 
-	sf::Int64 totalFrames = 0;
-	sf::Int32 elapsedFrames = 0;
-	sf::Int32 fpsUpdateFrequency = 10; //Hertz
-	sf::Int32 frameratelimit = 50;
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// General
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	sf::Color bgColor = sf::Color(50,50,50);
-	sf::Color alphaColor = sf::Color(0,0,0);
+	int gridsize_texture = 16;
+	sf::Color bgColor = sf::Color(176,228,252);
+	sf::Color alphaColor = sf::Color(0,0,0);		
 
-	sf::VideoMode fullscreenMode = modes[fullscreenModeIndex];
-	sf::VideoMode windowedMode = sf::VideoMode(width, height);
-	sf::RenderWindow window(windowedMode, "Hugoo");	
-	bool isFullscreen = false;
-	window.setFramerateLimit(frameratelimit);
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// World
+	/////////////////////////////////////////////////////////////////////////////////////////////////////	
 
-	sf::Image tileset;
-	if (!tileset.loadFromFile(std::string("src/textures/tileset.bmp").insert(0,mainpath))){
-		// error...
-	}
-	// interpretiere tileset ==> XML?
-	sf::IntRect stone = sf::IntRect(0,0,gridsize_texture,gridsize_texture);
-	sf::IntRect air = sf::IntRect(gridsize_texture,0,gridsize_texture,gridsize_texture);
-	sf::IntRect mud = sf::IntRect(gridsize_texture,gridsize_texture,gridsize_texture,gridsize_texture);
-	sf::IntRect grass = sf::IntRect(0,gridsize_texture,gridsize_texture,gridsize_texture);
+	int world_width = 320;
+	int world_height = 80;
 
-	sf::Image hugooImage;
+	std::vector<int> world; 
+
+	for (int iy=0;iy<world_height;iy++){
+		for (int ix=0;ix<world_width;ix++){			
+			if(iy<15){
+				world.emplace_back(0);			
+			}else if(iy==15){
+				world.emplace_back(3);
+			} else if(iy>15){
+				world.emplace_back(40);
+			}			
+		}
+	}	
+
+	myTileMap map;
+	if (!map.load(std::string("src/textures/minecraft_tiles16.png").insert(0,mainpath), sf::Vector2u(16, 16), &world, world_width, world_height))
+		return -1;
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Hugoo
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	sf::Image hugooImage;	
 	if (!hugooImage.loadFromFile(std::string("src/textures/hugoo.bmp").insert(0,mainpath))){
 		// error...
 	}	
-	hugooImage.createMaskFromColor(alphaColor);
+	hugooImage.createMaskFromColor(alphaColor);	
 
-	// interpretiere hugooImage ==> XML?
 	std::vector<sf::IntRect> trStandingCenter;
-	trStandingCenter.emplace_back(sf::IntRect(0,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
-	trStandingCenter.emplace_back(sf::IntRect(gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
+	trStandingCenter.emplace_back(sf::IntRect(0*gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
+	trStandingCenter.emplace_back(sf::IntRect(1*gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trStandingCenter.emplace_back(sf::IntRect(2*gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trStandingCenter.emplace_back(sf::IntRect(3*gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trStandingCenter.emplace_back(sf::IntRect(4*gridsize_texture,0*gridsize_texture,gridsize_texture,3*gridsize_texture));
@@ -138,8 +261,8 @@ int main()
 	mprStandingCenter.emplace_back(sf::Vector2f(0.f,0.f));
 
 	std::vector<sf::IntRect> trMovingRight;
-	trMovingRight.emplace_back(sf::IntRect(0,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
-	trMovingRight.emplace_back(sf::IntRect(gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
+	trMovingRight.emplace_back(sf::IntRect(0*gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
+	trMovingRight.emplace_back(sf::IntRect(1*gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trMovingRight.emplace_back(sf::IntRect(2*gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trMovingRight.emplace_back(sf::IntRect(3*gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
 	trMovingRight.emplace_back(sf::IntRect(4*gridsize_texture,3*gridsize_texture,gridsize_texture,3*gridsize_texture));
@@ -161,38 +284,34 @@ int main()
 	mprMovingRight.emplace_back(sf::Vector2f(2.f,0.f));
 	mprMovingRight.emplace_back(sf::Vector2f(2.f,0.f));
 
-	sf::Image screenImage;
-	screenImage.create(width,height,bgColor);
+	std::vector<sf::IntRect> trMovingLeft;
+	trMovingLeft.emplace_back(sf::IntRect(1*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
+	trMovingLeft.emplace_back(sf::IntRect(2*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
+	trMovingLeft.emplace_back(sf::IntRect(3*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
+	trMovingLeft.emplace_back(sf::IntRect(4*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
+	trMovingLeft.emplace_back(sf::IntRect(5*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
+	trMovingLeft.emplace_back(sf::IntRect(6*gridsize_texture,3*gridsize_texture,-gridsize_texture,3*gridsize_texture));
 
-	// generate World
-	int iy;
-	int ix;
+	std::vector<sf::Int32> fprMovingLeft;
+	fprMovingLeft.emplace_back(3);
+	fprMovingLeft.emplace_back(3);
+	fprMovingLeft.emplace_back(3);
+	fprMovingLeft.emplace_back(3);
+	fprMovingLeft.emplace_back(3);
+	fprMovingLeft.emplace_back(3);
 
-	for (iy=0;iy<height/gridsize_texture;iy++){
-		for (ix=0;ix<width/gridsize_texture;ix++){
-			sf::IntRect medium = air;			
-			if(iy==15){
-				medium = mud;
-			}
-			if(iy>15){
-				medium = stone;
-			}
-			screenImage.copy(tileset,ix*gridsize_texture,iy*gridsize_texture,medium,false);
-		}
-	}	
-
-	sf::Texture screenTexture;
-	if (!screenTexture.loadFromImage(screenImage)){
-		// error...
-	}
+	std::vector<sf::Vector2f> mprMovingLeft;
+	mprMovingLeft.emplace_back(sf::Vector2f(0.f,0.f));
+	mprMovingLeft.emplace_back(sf::Vector2f(0.f,0.f));
+	mprMovingLeft.emplace_back(sf::Vector2f(0.f,0.f));
+	mprMovingLeft.emplace_back(sf::Vector2f(-2.f,0.f));
+	mprMovingLeft.emplace_back(sf::Vector2f(-2.f,0.f));
+	mprMovingLeft.emplace_back(sf::Vector2f(-2.f,0.f));
 
 	sf::Texture hugooTexture;
 	if (!hugooTexture.loadFromImage(hugooImage)){
 		// error...
-	}
-
-	sf::Sprite screenSprite;
-	screenSprite.setTexture(screenTexture);
+	}		
 
 	myGameObject hugooSprite;
 	hugooSprite.initialize();
@@ -203,11 +322,11 @@ int main()
 	hugooSprite.setFramesPerRect(&fprStandingCenter);
 	hugooSprite.setMovesPerRect(&mprStandingCenter);	
 
-	sf::View view;
-	view.reset(sf::FloatRect(160, 120, 320, 240));
-	window.setView(view);
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Game Loop
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	sf::Clock clock;
+	view.setCenter(hugooSprite.getPosition());
 	while (window.isOpen()){
 		sf::Event event;
 		while (window.pollEvent(event)){
@@ -219,27 +338,49 @@ int main()
 				case sf::Keyboard::Escape: // Escape pressed : exit
 					window.close();
 					break;
-				case sf::Keyboard::L: // l pressed : move right 
+				case sf::Keyboard::L: // l pressed : Hugoo move right! 
 					hugooSprite.setTextureRects(&trMovingRight);
 					hugooSprite.setFramesPerRect(&fprMovingRight);
 					hugooSprite.setMovesPerRect(&mprMovingRight);
 					break;
-				case sf::Keyboard::J: // j pressed : stand center 
+				case sf::Keyboard::H: // h pressed : Hugoo move left! 
+					hugooSprite.setTextureRects(&trMovingLeft);
+					hugooSprite.setFramesPerRect(&fprMovingLeft);
+					hugooSprite.setMovesPerRect(&mprMovingLeft);
+					break;
+				case sf::Keyboard::J: // j pressed : Hugoo stop moving! 
 					hugooSprite.setTextureRects(&trStandingCenter);
 					hugooSprite.setFramesPerRect(&fprStandingCenter);
 					hugooSprite.setMovesPerRect(&mprStandingCenter);
 					break;
-				case sf::Keyboard::T: // T pressed : test
-					trStandingCenter[2] = sf::IntRect(0,3*gridsize_texture,gridsize_texture,3*gridsize_texture);
+				case sf::Keyboard::F: // F pressed : Hugoo do something!					
+					std::cout << map.changeTile(hugooSprite.getPosition(),12) <<std::endl;
 					break;
-				case sf::Keyboard::F11: // F11 pressed: fullscreen toggle
+				case sf::Keyboard::T: // T pressed : toggle following					
+					toggleViewFollowHugoo = !toggleViewFollowHugoo;
+					break;
+				case sf::Keyboard::W:
+					toggleViewFollowHugoo = false;
+					view.move(0.f,-8.f);
+					break;
+				case sf::Keyboard::S: 
+					toggleViewFollowHugoo = false;
+					view.move(0.f,8.f);
+					break;
+				case sf::Keyboard::A: 
+					toggleViewFollowHugoo = false;
+					view.move(-8.f,0.f);
+					break;
+				case sf::Keyboard::D:
+					toggleViewFollowHugoo = false;
+					view.move(8.f,0.f);
+					break;
+				case sf::Keyboard::F11: // F11 pressed: toggle fullscreen 
 					if (!isFullscreen) {
 						window.close();
 						window.create(fullscreenMode,"Hugoo",sf::Style::Fullscreen);
 						window.setFramerateLimit(frameratelimit);
-						isFullscreen = true;
-						//screenSprite.setScale(sf::Vector2f(2.f,2.f));
-						//hugooSprite.setScale(sf::Vector2f(2.f,2.f));
+						isFullscreen = true;						
 					}else {
 						window.close();
 						window.create(windowedMode,"Hugoo");
@@ -247,32 +388,19 @@ int main()
 						isFullscreen = false;
 					}
 					break;
-				default:
-					//game->handleKeyCode(event.key.code);
+				default:					
 					break;
 				}
 			} else if(event.type == sf::Event::KeyReleased) {
 				std::cout << "isFullscreen: " << isFullscreen << std::endl;
 			}
 		}
-		window.clear();
-		window.draw(screenSprite);
+		window.clear(bgColor);
+		if(toggleViewFollowHugoo){view.setCenter(hugooSprite.getPosition());}
+		window.setView(view);
+		window.draw(map);
 		hugooSprite.update();
 		window.draw(hugooSprite);
-
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// this code should remind me to implement a VERTEX based tile engine
-		sf::Color testColor = sf::Color(220,150,150);
-		sf::Vertex vertices[] =
-		{
-			sf::Vertex(sf::Vector2f(  0,   0), testColor, sf::Vector2f( 0,  0)),
-			sf::Vertex(sf::Vector2f(  0, 6*gridsize_texture), testColor, sf::Vector2f( 0, 3*gridsize_texture)),
-			sf::Vertex(sf::Vector2f(2*gridsize_texture, 6*gridsize_texture), testColor, sf::Vector2f(gridsize_texture, 3*gridsize_texture)),
-			sf::Vertex(sf::Vector2f(2*gridsize_texture,   0), testColor, sf::Vector2f(gridsize_texture,  0))
-		};
-		// draw it
-		window.draw(vertices, 4, sf::Quads,sf::RenderStates(&hugooTexture));
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		totalFrames++;
 		elapsedFrames++;
@@ -286,8 +414,6 @@ int main()
 		}
 		window.setView(window.getDefaultView());
 		window.draw(text);
-		window.setView(view);
-
 		window.display();
 	}
 	return 0;
